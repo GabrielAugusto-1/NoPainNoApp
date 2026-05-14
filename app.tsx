a4f7ca4fa37ba0b681e8458c87ac697da46a9246
Gabriel Augusto Firmino dos Santos <gabrielaugus6@gmail.com>
2026-05-13 21:52:14 -0300

    Add files via upload

--- app.tsx
+++ app.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import RootStack from "./app/(tabs)/index";
 
-export default App(){
+export default function App(){
     return(
         <NavigationContainer>
             <RootStack/>
